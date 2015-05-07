a0de3adc54440fa134a24964c647e3acc59e21e9
Thyago Barbosa Rodrigues <thyagobr@gmail.com>
2015-05-07 15:35:34 -0300

    Adds support for www.thaisagalvao.com.br

--- klipkrawler/spiders/spiderphant.py
+++ klipkrawler/spiders/spiderphant.py
@@ -13,28 +13,35 @@ import pdb
 
 class SpiderphantSpider(CrawlSpider):
     name = "spiderphant"
-    allowed_domains = ['www.tribunadonorte.com.br', 'blog.tribunadonorte.com.br']
+    allowed_domains = ['www.tribunadonorte.com.br',
+                       'blog.tribunadonorte.com.br', 
+                       'www.thaisagalvao.com.br']
     start_urls = [
-        'http://www.tribunadonorte.com.br'
+        'http://www.tribunadonorte.com.br',
+        'http://www.thaisagalvao.com.br'
     ]
 
     rules = [
-        Rule(LinkExtractor(
-            allow=['/noticia/']),
-            callback='parse_tribuna'
-        )
+        Rule(LinkExtractor(allow=['/noticia/', r'/\d{4}\/\d{2}\/\d{2}/']), callback='parse_tribuna'),
+   #     Rule(LinkExtractor(deny=['cadastro.tribunadonorte.com.br']))
     ]
 
-    def scrape_published_date(self, source, response):
-        if source == "tribunadonorte":
+    def scrape_published_date(self, response, published_date):
+        if "tribunadonorte.com.br" in response.url:
             pubdate = response.css('section[id=r-main] section[id=content] header small').extract()
             return pubdate[0] if pubdate else None
+        else:
+            return published_date.strftime('%d/%m/%Y')
 
-    def scrape_images(self, source, image_list):
-        if source == "tribunadonorte":
+    def scrape_images(self, response, image_list):
+        if "tribunadonorte.com.br" in response.url:
             return filter(lambda image:'arquivos' in image, image_list) 
+        else:
+            return image_list
 
     def parse_tribuna(self, response):
+        #if response.url == "http://cadastro.tribunadonorte.com.br/leitor/entrar":
+        #    return None
         article = Article(response.url)
         article.download()
         article.parse()
@@ -42,8 +49,8 @@ class SpiderphantSpider(CrawlSpider):
         item['title'] = article.title.encode('utf-8')
         item['text'] = article.text.strip()
         item['url'] = response.url
-        item['published_date'] = self.scrape_published_date("tribunadonorte", response)
-        item['images'] = self.scrape_images("tribunadonorte", article.images)
+        item['published_date'] = self.scrape_published_date(response, article.publish_date)
+        item['images'] = self.scrape_images(response, article.images)
         item['videos'] = article.movies
         with open('output.txt', 'a') as f:
             for key, value in item.iteritems():
@@ -52,6 +59,5 @@ class SpiderphantSpider(CrawlSpider):
                     value = ""
                 line = key + ' = ' + value
                 #print('%s = %s' % (key, value), file=f)
-                pdb.set_trace()
                 f.write(line + '\n')
 
